31a8e43d00378877ba0fa99224275e95814a959d
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-24 21:11:59 +0100

    Carmelo Anthony

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "pages#home"
-  get "/about", to: "pages#about"
-  get "/contact", to: "pages#contact"
-  get "/members", to: "pages#members"
+  get "/about", to: "pages#about", as: :about
+  get "/contact", to: "pages#contact", as: :contact
+  get "/members", to: "pages#members", as: :members
 end
