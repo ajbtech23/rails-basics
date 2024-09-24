1c6e4fe4c71685e459af6a708abbfbf97b5a561c
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-24 20:29:33 +0100

    Doing well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,20 @@
 class PagesController < ApplicationController
   def home
+    @homes_away_from_home = [ "Dubai", "Abu Dhabi", "Beijing", "Dalma" ]
   end
 
   def about
     @about_me = "Living La Vida Loca"
+    @about_me_details = {
+      name: "Joshua",
+      age: 30,
+      fave_sport: "basketball"
+    }
   end
 
   def contact
     @contact_me = "If you like me 079 me"
+    @contact_numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
   end
 
   def members
